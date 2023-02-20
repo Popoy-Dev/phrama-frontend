cffc4bcdb304be461db8b8e1063b420d68c20d18
Kua <popoykua28@gmail.com>
2023-02-20 21:02:07 +0800

    Add total discount

--- components/order/CustomerOrder.tsx
+++ components/order/CustomerOrder.tsx
@@ -38,18 +38,36 @@ const CustomerOrder = ({
   isRemoveItem,
 }: any) => {
   const [removetotalQuantity, setRemoveTotalQuantity] = useState(0)
+  const [removetotalDiscount, setRemoveTotalDiscount] = useState(0)
 
   const getTotal = () => {
+    if(customerOrder.length === 0) {
+      setRemoveTotalDiscount(0)
+      setRemoveTotalQuantity(0)
+    }
     setRemoveTotalQuantity(
       customerOrder.reduce(
         (acc: any, obj: any) => acc + parseInt(obj.order_quantity),
         0
       )
     )
+
+      customerOrder.map((data: any) => {
+          if(data.discounted) {
+            setRemoveTotalDiscount(
+              customerOrder.reduce(
+                (acc: any, obj: any) => acc + (data.srp_price * (20 / 100) *parseInt(data?.order_quantity)),
+                0
+              )
+            )
+          }
+      })
   }
+
   useEffect(() => {
     getTotal()
   }, [customerOrder, isRemoveItem])
+  
   return (
     <TableContainer>
       <Table variant='striped' colorScheme='purple'>
@@ -86,10 +104,7 @@ const CustomerOrder = ({
                   </Td>
                   <Td>
                     {data.discounted
-                      ? parseInt(data?.order_quantity) * data.srp_price -
-                        data.srp_price *
-                          (20 / 100) *
-                          parseInt(data?.order_quantity)
+                      ? (parseInt(data?.order_quantity) * data.srp_price) - ((data.srp_price *(20 / 100)) * parseInt(data?.order_quantity))
                       : (
                           parseInt(data?.order_quantity) * data.srp_price
                         ).toFixed(2)}{' '}
@@ -114,7 +129,7 @@ const CustomerOrder = ({
             <Th></Th>
             <Th></Th>
             <Th>Total quantity - {removetotalQuantity} pieces</Th>
-            <Th>Total Discount</Th>
+            <Th>Total Discount - {removetotalDiscount.toFixed(2)}PHP</Th>
             <Th isNumeric>Total Amount</Th>
           </Tr>
         </Tfoot>
