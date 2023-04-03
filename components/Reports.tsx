4f0ffb72e3d1c02f83f921666a8d52f7b7d7eaba
Kua <popoykua28@gmail.com>
2023-04-03 20:22:35 +0800

    Remove error of ant design package

--- components/Reports.tsx
+++ components/Reports.tsx
@@ -1,29 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import { DateRangePicker } from 'react-date-range';
+import React, { useState, useEffect } from 'react'
+import { DateRangePicker } from 'react-date-range'
 import { Box, SimpleGrid } from '@chakra-ui/react'
-
-import { Line } from '@ant-design/plots';
+import dynamic from 'next/dynamic'
 import CustomerOrderReport from './reports/CustomerOrderReport'
-import 'react-date-range/dist/styles.css'; // main style file
-import 'react-date-range/dist/theme/default.css'; // theme css file
+import { supabase } from '../supabaseClient'
+import 'react-date-range/dist/styles.css' // main style file
+import 'react-date-range/dist/theme/default.css' // theme css file
+const Line = dynamic(
+  () => import('@ant-design/plots').then(({ Line }) => Line),
+  { ssr: false }
+)
 
 const Reports = () => {
-  const [data, setData] = useState([]);
-  const [selectedStartDate, setSelectedStartDate] = useState<Date>(new Date());
-  const [selectedEndDate, setSelectedEndDate] = useState<Date>(new Date());
+  const [data, setData] = useState([])
+  const [selectedStartDate, setSelectedStartDate] = useState<Date>(new Date())
+  const [selectedEndDate, setSelectedEndDate] = useState<Date>(new Date())
   useEffect(() => {
-    asyncFetch();
-  }, []);
+    asyncFetch()
+  }, [])
 
   const asyncFetch = () => {
-    fetch('https://gw.alipayobjects.com/os/bmw-prod/1d565782-dde4-4bb6-8946-ea6a38ccf184.json')
+    fetch(
+      'https://gw.alipayobjects.com/os/bmw-prod/1d565782-dde4-4bb6-8946-ea6a38ccf184.json'
+    )
       .then((response) => response.json())
       .then((json) => setData(json))
       .catch((error) => {
-        console.log('fetch data failed', error);
-      });
-  };
+        console.log('fetch data failed', error)
+      })
+  }
   // console.log('data', data)
   const config: any = {
     data,
@@ -34,14 +39,20 @@ const Reports = () => {
       // type: 'timeCat',
       tickCount: 100,
     },
-  };
+  }
 
-  const handleSelect = (date: any) =>{
+  const handleSelect = async (date: any) => {
     const endDate = date.selection.endDate
     const startDate = date.selection.startDate
     setSelectedStartDate(startDate)
     setSelectedEndDate(endDate)
-    console.log(date.selection.startDate); // native Date object
+    const { data, error } = await supabase
+      .from('orders')
+      .select()
+      .rangeLt('created_at', '[2023-04-03 , 2023-04-03)')
+
+    console.log(date.selection.startDate) // native Date object
+    console.log('data', data) // native Date object
   }
   const selectionRange = {
     startDate: selectedStartDate,
