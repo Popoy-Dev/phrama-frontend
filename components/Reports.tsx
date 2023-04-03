40a88e4476ee7904566d7aebdf2dae71ba498d4c
Kua <popoykua28@gmail.com>
2023-04-03 15:21:59 +0800

    Add date range

--- components/Reports.tsx
+++ components/Reports.tsx
@@ -1,68 +1,68 @@
-import React from 'react'
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js'
-import { Bar } from 'react-chartjs-2'
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { DateRangePicker } from 'react-date-range';
 import { Box, SimpleGrid } from '@chakra-ui/react'
+
+import { Line } from '@ant-design/plots';
 import CustomerOrderReport from './reports/CustomerOrderReport'
-
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
-
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top' as const,
-    },
-    title: {
-      display: true,
-      text: 'Fayne Pharma Sell Chart',
-    },
-  },
-}
-
-const labels = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-]
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Sell Sheet',
-      data: labels.map(() => Math.floor(Math.random() * 100)),
-      backgroundColor: 'rgba(61, 50, 52, 0.5)',
-    },
-  ],
-}
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 
 const Reports = () => {
+  const [data, setData] = useState([]);
+  const [selectedStartDate, setSelectedStartDate] = useState<Date>(new Date());
+  const [selectedEndDate, setSelectedEndDate] = useState<Date>(new Date());
+  useEffect(() => {
+    asyncFetch();
+  }, []);
+
+  const asyncFetch = () => {
+    fetch('https://gw.alipayobjects.com/os/bmw-prod/1d565782-dde4-4bb6-8946-ea6a38ccf184.json')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => {
+        console.log('fetch data failed', error);
+      });
+  };
+  // console.log('data', data)
+  const config: any = {
+    data,
+    padding: 'auto',
+    xField: 'Date',
+    yField: 'scales',
+    xAxis: {
+      // type: 'timeCat',
+      tickCount: 100,
+    },
+  };
+
+  const handleSelect = (date: any) =>{
+    const endDate = date.selection.endDate
+    const startDate = date.selection.startDate
+    setSelectedStartDate(startDate)
+    setSelectedEndDate(endDate)
+    console.log(date.selection.startDate); // native Date object
+  }
+  const selectionRange = {
+    startDate: selectedStartDate,
+    endDate: selectedEndDate,
+    key: 'selection',
+  }
+  console.log('selectedStartDate', selectedStartDate)
+  console.log('selectedEndDate', selectedEndDate)
   return (
     <SimpleGrid columns={2} spacing={10}>
       <Box bg='white' height='80px'>
         {' '}
         <CustomerOrderReport />{' '}
       </Box>
-      <Box bg='white' w='100%' p={4} color='white' borderRadius='md'>
-        <Bar options={options} data={data} />
-      </Box>
+      <DateRangePicker
+        ranges={[selectionRange]}
+        onChange={handleSelect}
+        rangeColors={['red']}
+        showDateDisplay
+      />
+      <Line {...config} />
     </SimpleGrid>
   )
 }
