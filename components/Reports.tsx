4a658eb9030c43a38f7123c24a45bb125e1283a1
Kua <popoykua28@gmail.com>
2023-04-04 20:24:23 +0800

    Add chart real value

--- components/Reports.tsx
+++ components/Reports.tsx
@@ -12,32 +12,27 @@ const Line = dynamic(
 )
 
 const Reports = () => {
-  const [data, setData] = useState([])
+  const [data, setTotalRangeDateAmount] = useState<any>([{Date: '2010-01', scales: 1998},
+  {Date: '2010-02', scales: 1850},
+  {Date: '2010-02', scales: 1850},
+])
+  const [totalAmount, setTotalAmount] = useState<any>(0)
   const [selectedStartDate, setSelectedStartDate] = useState<Date>(new Date())
   const [selectedEndDate, setSelectedEndDate] = useState<Date>(new Date())
-  useEffect(() => {
-    asyncFetch()
-  }, [])
 
-  const asyncFetch = () => {
-    fetch(
-      'https://gw.alipayobjects.com/os/bmw-prod/1d565782-dde4-4bb6-8946-ea6a38ccf184.json'
-    )
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => {
-        console.log('fetch data failed', error)
-      })
-  }
-  // console.log('data', data)
+  // console.log('data fake', data)
+
+  console.log('selectedRangeData', data)
+  console.log('data', data)
+  
   const config: any = {
     data,
     padding: 'auto',
     xField: 'Date',
     yField: 'scales',
     xAxis: {
-      // type: 'timeCat',
-      tickCount: 100,
+      type: 'timeCat',
+      tickCount: 30,
     },
   }
 
@@ -58,14 +53,41 @@ const Reports = () => {
       .toISOString()
       .substr(0, 10)
 
-      const start1 = '2023-03-01T00:00:00.000Z';
-const end1 = '2023-03-31T23:59:59.999Z';
-    const { data, error } = await supabase
+    const start1 = '2023-03-01T00:00:00.000Z'
+    const end1 = '2023-03-31T23:59:59.999Z'
+    const { data , error } :any = await supabase
       .from('orders')
       .select()
       .gte('created_at', start)
       .lte('created_at', end)
       .order('created_at', { ascending: false })
+      console.log('data', data)
+
+      let a: any= []
+      data.forEach((element: any) => {
+        const b =  {Date: element.created_at, scales: element.order_totals_details.totalAmount}
+        a.push(b)
+      });
+      console.log('a', a)
+     let lineGraphData = a.reduce((acc :any, obj: any) =>  {
+        let Date = obj.Date
+        let scales = obj.scales 
+
+        let existingTransactionDate = acc.find((ac: any) => ac.Date === Date)
+
+        if(existingTransactionDate) {
+          existingTransactionDate.scales += scales
+        }else {
+         acc.push({Date, scales})
+        }
+
+        return acc
+      }, [])
+
+      console.log('line graph', lineGraphData)
+      setTotalRangeDateAmount(lineGraphData)
+      setTotalAmount(data?.reduce((acc: any, obj: any) => acc + obj.order_totals_details.totalAmount, 0))
+      // console.log('data', data)
   }
   const selectionRange = {
     startDate: selectedStartDate,
