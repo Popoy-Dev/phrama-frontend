5f97d63fe9c47b0561fc7775219288dbb876c2d3
Kua <popoykua28@gmail.com>
2023-04-15 16:50:22 +0800

    Add Next and previous button

--- components/invetory/InventoryList.tsx
+++ components/invetory/InventoryList.tsx
@@ -12,49 +12,61 @@ import {
   Button,
   Badge,
   Text,
+  Flex,
+  Spacer,
 } from '@chakra-ui/react'
-
 import { supabase } from '../../supabaseClient'
 import InventoryModal from './InventoryModal'
 
 interface Inventory {
   id: number
-  product_name: string,
+  product_name: string
   batch_number: number
   manufacture_price: number
   srp_price: number
   quantity: number
   is_vatable: boolean
-  expiry_date: Date,
+  expiry_date: Date
   ordered_quantity: number
 }
-const InventoryList = ({reloadList, inventoryData, getInventory}: any) => {
+const InventoryList = ({ reloadList, inventoryData, getInventory }: any) => {
   const [selectedInventory, setSelectedInventory] = useState<Inventory[]>([])
   const [isOpen, setIsOpen] = useState(false)
   const initialRef = React.useRef(null)
   const finalRef = React.useRef(null)
+  const [currentPage, setCurrentPage] = useState(1)
   const onOpen = () => {
     setIsOpen(true)
   }
-  const onClose = (data:[])  => {
-    if(data) {
+  const onClose = (data: []) => {
+    if (data) {
       getInventory()
     }
     setIsOpen(false)
   }
 
+  function handlePrevPage() {
+    setCurrentPage((prevPage) => prevPage - 1)
+  }
 
+  function handleNextPage() {
+    setCurrentPage((prevPage) => prevPage + 1)
+  }
+
+  function paginate(array: [], page_size: any, page_number: any) {
+    return array.slice((page_number - 1) * page_size, page_number * page_size)
+  }
+  const paginatedData = paginate(inventoryData, 2, currentPage)
 
   const handleEditInventory = async (id: number) => {
-    const {data, error } = await supabase
-    .from('inventory')
-    .select(
-    )
-  .eq('id', id)
-  if(data) {
-    setIsOpen(true)         
-    setSelectedInventory(data[0])
-  }
+    const { data, error } = await supabase
+      .from('inventory')
+      .select()
+      .eq('id', id)
+    if (data) {
+      setIsOpen(true)
+      setSelectedInventory(data[0])
+    }
   }
   return (
     <TableContainer>
@@ -70,12 +82,11 @@ const InventoryList = ({reloadList, inventoryData, getInventory}: any) => {
             <Th>SOH</Th>
             <Th>Is Vatable</Th>
             <Th>Expiry Date</Th>
-  
           </Tr>
         </Thead>
         <Tbody>
           {inventoryData &&
-            inventoryData?.map((data: Inventory, i: number) => {
+            paginatedData?.map((data: Inventory, i: number) => {
               return (
                 <Tr key={i}>
                   <Td>{data.product_name}</Td>
@@ -83,25 +94,59 @@ const InventoryList = ({reloadList, inventoryData, getInventory}: any) => {
                   <Td>{`₱ ${data.manufacture_price.toFixed(2)}`}</Td>
                   <Td>{`₱ ${data.srp_price.toFixed(2)}`}</Td>
                   <Td>{data.quantity} pieces</Td>
-                  <Td>{data.quantity-data.ordered_quantity === 0 ? <Text as="mark">{data.quantity-data.ordered_quantity} pieces</Text> : <Text>{data.quantity-data.ordered_quantity} pieces</Text>}  </Td>
-                  <Td>{data.is_vatable ? <Badge colorScheme='green'>With VAT</Badge> : <Badge colorScheme='red'>Non VAT</Badge>}</Td>
+                  <Td>
+                    {data.quantity - data.ordered_quantity === 0 ? (
+                      <Text as='mark'>
+                        {data.quantity - data.ordered_quantity} pieces
+                      </Text>
+                    ) : (
+                      <Text>
+                        {data.quantity - data.ordered_quantity} pieces
+                      </Text>
+                    )}{' '}
+                  </Td>
+                  <Td>
+                    {data.is_vatable ? (
+                      <Badge colorScheme='green'>With VAT</Badge>
+                    ) : (
+                      <Badge colorScheme='red'>Non VAT</Badge>
+                    )}
+                  </Td>
                   <Td>{new Date(`${data.expiry_date}`).toUTCString()}</Td>
                   <Td>
-                    <Button colorScheme='yellow' onClick={() =>handleEditInventory(data.id)}>Edit</Button>
+                    <Button
+                      colorScheme='yellow'
+                      onClick={() => handleEditInventory(data.id)}
+                    >
+                      Edit
+                    </Button>
                   </Td>
                 </Tr>
               )
             })}
         </Tbody>
-  
+        <div>
+          <Flex>
+            <button disabled={currentPage === 1} onClick={handlePrevPage}>
+              Prev
+            </button>
+            <Spacer />
+            <button
+              disabled={currentPage === Math.ceil(inventoryData.length / 2)}
+              onClick={handleNextPage}
+            >
+              Next
+            </button>
+          </Flex>
+        </div>
       </Table>
       <InventoryModal
-              initialFocusRef={initialRef}
-              finalFocusRef={finalRef}
-              isOpen={isOpen}
-              onClose={onClose}
-              updateData={selectedInventory}
-            />
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+        updateData={selectedInventory}
+      />
     </TableContainer>
   )
 }
